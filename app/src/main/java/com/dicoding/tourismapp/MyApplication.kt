a3af6432037e6041a47f823794ae57971e952372
Asep Sutisna <sutisnaasep323@gmail.com>
2022-03-09 10:04:01 +0700

    Tourism Dagger

---
+++ app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
@@ -0,0 +1,21 @@
+package com.dicoding.tourismapp
+
+import android.app.Application
+import com.dicoding.tourismapp.core.di.*
+import com.dicoding.tourismapp.di.AppComponent
+import com.dicoding.tourismapp.di.DaggerAppComponent
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+open class MyApplication : Application() {
+
+    private val coreComponent: CoreComponent by lazy {
+        DaggerCoreComponent.factory().create(applicationContext)
+    }
+
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.factory().create(coreComponent)
+    }
+}
